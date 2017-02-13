3fdfc0f7bcb9256c338969125ef27de4e36bf508
bookybooky <lonelybooky@gmail.com>
2017-02-12 22:40:24 -0600

    update pageRecIterator

--- A2/Main/DatabaseTable/source/MyDB_PageRecIterator.cc
+++ A2/Main/DatabaseTable/source/MyDB_PageRecIterator.cc
@@ -3,19 +3,31 @@
 
 #include <iostream>
 #include "MyDB_PageRecIterator.h"
+//#include "MyDB_TableReaderWriter.h"
 
 using namespace std;
-
-MyDB_PageRecIterator::MyDB_PageRecIterator (MyDB_RecordPtr iterateIntoMe, MyDB_PageHandle myPageHandle){
-    this->iterateIntoMe=iterateIntoMe;
-    this->myPageHandle=myPageHandle;
-
+// constructor
+MyDB_PageRecIterator::MyDB_PageRecIterator(MyDB_RecordPtr myRec, MyDB_PageHandle myPage){
+    this->myPage = myPage;
+    this->myRec = myRec;
+    readBytesLength = 0;
 }
+
 void MyDB_PageRecIterator::getNext(){
-    
+    // MyDB_PageHandle myHandle = myBuffer->getPage(myTable,pageID);
+    curr_pos = &(((char*)myHandle->getBytes()) [readBytesLength]);
+    curr_pos = myRec->fromBinary(curr_pos);
+    readBytesLength += myRec->getBinarySize(); // get bytesLength of this rec.
 }
 
 bool MyDB_PageRecIterator::hasNext(){
-    
+    cout<<"This is a page."<<endl;
+    current_pos = &(((char*)myHandle->getBytes()) [readBytesLength]);
+    pageSize = myBuffer->getPageSize();
+    if(readBytesLength >= pageSize){
+        cout<<"reach the end"<<pageSize<<" "<<readBytesLength<<endl;// Testing
+        return false;
+    }
+    return true;
 }
 #endif
